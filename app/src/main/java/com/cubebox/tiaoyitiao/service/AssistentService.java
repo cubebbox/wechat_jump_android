ceb156db8d5bc1983c76684a6c0825843302b443
luozisong <luozisong@126.com>
2018-01-04 10:17:05 +0800

    fix bugs

--- app/src/main/java/com/cubebox/tiaoyitiao/service/AssistentService.java
+++ app/src/main/java/com/cubebox/tiaoyitiao/service/AssistentService.java
@@ -1,10 +1,13 @@
 package com.cubebox.tiaoyitiao.service;
 
+import android.app.Notification;
+import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.os.Handler;
+import android.os.IBinder;
 import android.os.Message;
 import android.util.Log;
 import android.view.Gravity;
@@ -20,6 +23,7 @@ import com.cubebox.tiaoyitiao.AssistentView;
 import com.cubebox.tiaoyitiao.R;
 import com.cubebox.tiaoyitiao.data.ConstantsPreference;
 import com.cubebox.tiaoyitiao.utils.BroadcastUtil;
+import com.cubebox.tiaoyitiao.utils.CycleUtile;
 import com.cubebox.tiaoyitiao.utils.DensityUtil;
 import com.cubebox.tiaoyitiao.utils.PreferencesManager;
 import com.cubebox.tiaoyitiao.utils.ScreenUtil;
@@ -45,10 +49,17 @@ public class AssistentService extends BaseService {
 
     //要引用的布局文件.
     LinearLayout toucherLayout;
+    LinearLayout toucherLayout2;
     //布局参数.
     WindowManager.LayoutParams params;
     //实例化的WindowManager.
     WindowManager windowManager;
+    private CycleUtile cycleUtile;
+
+    int selectBtn = 1;//1红色按钮 2.蓝色按钮
+    float rate = 1f;
+    int MAX_TIME = 3;//自动跳倒计时单位秒
+    int time = MAX_TIME;
 
     @Override
     public void initObject() {
@@ -64,6 +75,11 @@ public class AssistentService extends BaseService {
 //        if (mMyServiceConnection == null) {
 //            mMyServiceConnection = new MyServiceConnection();
 //        }
+
+        //赋值WindowManager&LayoutParam.
+        params = new WindowManager.LayoutParams();
+        windowManager = (WindowManager) getApplication().getSystemService(Context.WINDOW_SERVICE);
+        cycleUtile = new CycleUtile();
     }
 
     @Override
@@ -91,33 +107,65 @@ public class AssistentService extends BaseService {
             handler.sendEmptyMessage(1);
         }
     };
-
     BroadcastReceiver close = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             handler.sendEmptyMessage(2);
         }
     };
+
+    BroadcastReceiver hide = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            handler.sendEmptyMessage(3);
+        }
+    };
+    BroadcastReceiver show = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            handler.sendEmptyMessage(1);
+        }
+    };
+
+    /**
+     * 1.展示  2. 关闭 3.隐藏
+     */
     Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             switch (msg.what) {
                 case 1:
+                    if (windowManager != null && toucherLayout2 != null) {
+                        windowManager.removeViewImmediate(toucherLayout2);
+                    }
+                    toucherLayout2 = null;
                     createToucher();
                     break;
                 case 2:
                     if (windowManager != null && toucherLayout != null) {
-                        windowManager.removeView(toucherLayout);
+//                        windowManager.removeView(toucherLayout);
+                        windowManager.removeViewImmediate(toucherLayout);
                     }
+                    if (windowManager != null && toucherLayout2 != null) {
+                        windowManager.removeViewImmediate(toucherLayout2);
+                    }
+                    toucherLayout = null;
+                    toucherLayout2 = null;
+                    break;
+                case 3:
+                    if (windowManager != null && toucherLayout != null) {
+//                        windowManager.removeView(toucherLayout);
+                        windowManager.removeViewImmediate(toucherLayout);
+                    }
+                    toucherLayout = null;
+                    createHideView();
                     break;
             }
         }
     };
 
+
     private void createToucher() {
-        //赋值WindowManager&LayoutParam.
-        params = new WindowManager.LayoutParams();
-        windowManager = (WindowManager) getApplication().getSystemService(Context.WINDOW_SERVICE);
         //设置type.系统提示型窗口，一般都在应用程序窗口之上.
         params.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
         //设置效果为背景透明.
@@ -140,6 +188,7 @@ public class AssistentService extends BaseService {
         LayoutInflater inflater = LayoutInflater.from(getApplication());
         //获取浮动窗口视图所在布局.
         toucherLayout = (LinearLayout) inflater.inflate(R.layout.view_assistent, null);
+
         //添加toucherlayout
         windowManager.addView(toucherLayout, params);
 
@@ -167,13 +216,28 @@ public class AssistentService extends BaseService {
             @Override
             public void onClick(View v) {
                 execShellCmd("input swipe 200 200 400 200 " + (int) (rate * assistentView.getDistance()));
+                cycleUtile.startCycle(1000, new CycleUtile.onCycleListener() {
+                    @Override
+                    public void onCycle() {
+                        if (time > 0) {//为避免连续点击
+                            time -= 1;
+                            jump.setText("自动跳(" + time + ")");
+                            jump.setClickable(false);
+                        } else {
+                            time = MAX_TIME;
+                            cycleUtile.stopCycle();
+                            jump.setText("自动跳");
+                            jump.setClickable(true);
+                        }
+                    }
+                });
             }
         });
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                rate += 0.02f;
+                rate += 0.01f;
                 PreferencesManager.getInstance(getApplicationContext()).putString(PreferencesManager.PRE_RATE, rate + "");
                 tvRate.setText("敏感度：" + rate);
             }
@@ -182,7 +246,7 @@ public class AssistentService extends BaseService {
         reduce.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                rate -= 0.02f;
+                rate -= 0.01f;
                 PreferencesManager.getInstance(getApplicationContext()).putString(PreferencesManager.PRE_RATE, rate + "");
                 tvRate.setText("敏感度：" + rate);
             }
@@ -226,22 +290,54 @@ public class AssistentService extends BaseService {
         small.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (layout.getVisibility() == View.VISIBLE) {
-                    layout.setVisibility(View.GONE);
-                    small.setText("展开");
-                } else {
-                    layout.setVisibility(View.VISIBLE);
-                    small.setText("缩小");
-                }
-
+                handler.sendEmptyMessage(3);
             }
         });
 
     }
 
+    /**
+     * 展示缩小的界面
+     */
+    private void createHideView() {
+        //设置type.系统提示型窗口，一般都在应用程序窗口之上.
+        params.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
+        //设置效果为背景透明.
+        params.format = PixelFormat.RGBA_8888;
+        //设置flags.不可聚焦及不可使用按钮对悬浮窗进行操控.
+        params.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
 
-    int selectBtn = 1;//1红色按钮 2.蓝色按钮
-    float rate = 1f;
+        //设置窗口初始停靠位置.
+        params.gravity = Gravity.RIGHT | Gravity.BOTTOM;
+        params.x = 0;
+        params.y = ScreenUtil.getScreenHeight(this) - DensityUtil.dip2px(this, 80);
+
+        //设置悬浮窗口长宽数据.
+        //注意，这里的width和height均使用px而非dp.这里我偷了个懒
+        //如果你想完全对应布局设置，需要先获取到机器的dpi
+        //px与dp的换算为px = dp * (dpi / 160).
+        params.width = DensityUtil.dip2px(this, 100);
+        params.height = DensityUtil.dip2px(this, 50);
+
+        LayoutInflater inflater = LayoutInflater.from(getApplication());
+        //获取浮动窗口视图所在布局.
+        toucherLayout2 = (LinearLayout) inflater.inflate(R.layout.view_hide, null);
+        //添加toucherlayout
+        windowManager.addView(toucherLayout2, params);
+
+        //主动计算出当前View的宽高信息.
+        toucherLayout2.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+
+        final Button show = (Button) toucherLayout2.findViewById(R.id.show);
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                windowManager.removeViewImmediate(toucherLayout2);
+                createToucher();
+            }
+        });
+    }
+
 
     /**
      * 执行shell命令
@@ -266,82 +362,31 @@ public class AssistentService extends BaseService {
         }
     }
 
-//    @Override
-//    public IBinder onBind(Intent intent) {
-//        return mBinder;
-//    }
-//
-//    //通过Aidl进行通讯
-//    private class localBinder extends MyProcessAIDL.Stub {
-//
-//        @Override
-//        public String getServiceName() throws RemoteException {
-//            return TAG;
-//        }
-//    }
-//
-//    //连接远程服务
-//    private class MyServiceConnection implements ServiceConnection {
-//
-//        @Override
-//        public void onServiceConnected(ComponentName name, IBinder service) {
-//            try {
-//                // 与远程服务通信
-//                MyProcessAIDL process = MyProcessAIDL.Stub.asInterface(service);
-//                Log.e(TAG, "连接" + process.getServiceName() + "服务成功");
-//            } catch (RemoteException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//
-//        @Override
-//        public void onServiceDisconnected(ComponentName name) {
-//            // RemoteException连接过程出现的异常，才会回调,unbind不会回调
-//            // 监测，远程服务已经死掉，则重启远程服务
-//            Log.e(TAG, "远程服务挂掉了,远程服务被杀死");
-//            // 启动远程服务
-//            startService(new Intent(AssistentService.this, KeepLiveService.class));
-//            // 绑定远程服务
-//            bindService(new Intent(AssistentService.this, KeepLiveService.class), mMyServiceConnection, Context.BIND_IMPORTANT);
-//        }
-//    }
-//
-//
-//    @Override
-//    public void onCycle() {
-//        boolean b = isServiceExisted(getApplicationContext(), "net.chofn.crm.service.KeepLiveService");
-//        if (!b) {
-//            Intent service = new Intent(getApplicationContext(), KeepLiveService.class);
-//            startService(service);
-//        }
-//
-//    }
-//
-//    public static class GrayInnerService extends Service {
-//
-//        @Override
-//        public void onCreate() {
-//            super.onCreate();
-//        }
-//
-//        @Override
-//        public int onStartCommand(Intent intent, int flags, int startId) {
-//            startForeground(GRAY_SERVICE_ID, new Notification());
-//            //stopForeground(true);
-//            stopSelf();
-//            return super.onStartCommand(intent, flags, startId);
-//        }
-//
-//        @Override
-//        public IBinder onBind(Intent intent) {
-//            throw new UnsupportedOperationException("Not yet implemented");
-//        }
-//
-//        @Override
-//        public void onDestroy() {
-//            super.onDestroy();
-//        }
-//    }
+    public static class GrayInnerService extends Service {
+
+        @Override
+        public void onCreate() {
+            super.onCreate();
+        }
+
+        @Override
+        public int onStartCommand(Intent intent, int flags, int startId) {
+            startForeground(GRAY_SERVICE_ID, new Notification());
+            //stopForeground(true);
+            stopSelf();
+            return super.onStartCommand(intent, flags, startId);
+        }
+
+        @Override
+        public IBinder onBind(Intent intent) {
+            throw new UnsupportedOperationException("Not yet implemented");
+        }
+
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+        }
+    }
 
 
     @Override
@@ -354,58 +399,8 @@ public class AssistentService extends BaseService {
         Log.e(TAG, TAG + "  onDestroy() executed");
         Intent localIntent = new Intent();
         localIntent.setClass(getApplicationContext(), AssistentService.class); //销毁时重新启动Service
+        handler.sendEmptyMessage(2);
         this.startService(localIntent);
     }
 
-//    /**
-//     * 开启守护服务
-//     */
-//    private void startKeepLiveService() {
-//        boolean b = isServiceExisted(getApplicationContext(), "net.chofn.crm.service.KeepLiveService");
-//        if (!b) {
-//            Intent service = new Intent(getApplicationContext(), KeepLiveService.class);
-//            startService(service);
-//        }
-//    }
-//
-//    /**
-//     * 开启任务服务
-//     */
-//    private void startTaskService() {
-//        boolean b = isServiceExisted(getApplicationContext(), "net.chofn.crm.service.TaskService");
-//        if (!b) {
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-//                Intent service = new Intent(getApplicationContext(), TaskService.class);
-//                startService(service);
-//            }
-//        }
-//    }
-//
-//    /**
-//     * 判断某个服务是否正在运行的方法
-//     *
-//     * @param context
-//     * @param className 是包名+服务的类名（例如：net.loonggg.testbackstage.TestService）
-//     * @return true代表正在运行，false代表服务没有正在运行
-//     */
-//    public static boolean isServiceExisted(Context context, String className) {
-//        ActivityManager activityManager = (ActivityManager) context
-//                .getSystemService(Context.ACTIVITY_SERVICE);
-//        List<ActivityManager.RunningServiceInfo> serviceList = activityManager
-//                .getRunningServices(Integer.MAX_VALUE);
-//
-//        if (!(serviceList.size() > 0)) {
-//            return false;
-//        }
-//
-//        for (int i = 0; i < serviceList.size(); i++) {
-//            ActivityManager.RunningServiceInfo serviceInfo = serviceList.get(i);
-//            ComponentName serviceName = serviceInfo.service;
-//
-//            if (serviceName.getClassName().equals(className)) {
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
 }
